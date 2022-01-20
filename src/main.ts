84874f52c41f95a3ea7b3c6551acb1ec6561b240
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-01-20 15:24:18 +0200

    Use swagger to document all APIs.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //* Add global middleware here
+  //? Setup Swagger here
+  const config = new DocumentBuilder()
+    .setTitle('Product Management 📃')
+    .setDescription('The product management API description')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger/api-doc', app, document);
 
   await app.listen(3000, () => {
     console.log('Server is listening on port 3000 💥');
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,13 +7,25 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Add new product' })
+  @ApiCreatedResponse({ description: 'Product added successfully' })
   async addProduct(
     @Body('title') prodTitle: string,
     @Body('desc') prodDescription: string,
@@ -28,16 +40,24 @@ export class ProductsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Fetch all products' })
+  @ApiOkResponse({ description: 'All products returned successfully' })
   async getAllProducts() {
     return await this.productsService.getProducts();
   }
 
   @Get(':prodId')
+  @ApiOperation({ summary: 'Fetch single products' })
+  @ApiOkResponse({ description: 'Product returned successfully' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   async getProduct(@Param('prodId') prodId: string) {
     return await this.productsService.getSingleProduct(prodId);
   }
 
   @Patch(':prodId')
+  @ApiOperation({ summary: 'Update product' })
+  @ApiOkResponse({ description: 'Product updated successfully' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   async updateProduct(
     @Param('prodId') prodId: string,
     @Body() prodData: { title: string; description: string; price: number },
@@ -46,6 +66,9 @@ export class ProductsController {
   }
 
   @Delete(':prodId')
+  @ApiOperation({ summary: 'Delete product' })
+  @ApiOkResponse({ description: 'Product delete successfully' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   async deleteProduct(@Param('prodId') prodId: string) {
     return await this.productsService.deleteProduct(prodId);
   }
